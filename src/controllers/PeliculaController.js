f54cc53f82452612972bfe4cb29cfe84f98a5c6a
Mauricio Koifman <111393008+Maurikoif@users.noreply.github.com>
2023-05-19 11:46:20 -0300

    Update PeliculaController.js

--- src/controllers/PeliculaController.js
+++ src/controllers/PeliculaController.js
@@ -10,6 +10,23 @@ router.get('', async(req,res)=>
     const pelicula = await peliculaService.ObtenerPeliculas(req.query)
     return res.status(200).json(pelicula)
 })
+router.post('',async(req,res)=>
+{
+    console.log('Post')
+    const pelicula = await peliculaService.AgregarPelicula(req.body)
+    return res.status(200).json(pelicula)
+})
+router.delete('/:id', async(req,res)=>
+{
+    console.log('Delete')
+    const pelicula = await peliculaService.EliminarPelicula(req.params.id)
+    return res.status(200).json(pelicula)
+})
 
-
+router.put('/:id', async(req,res)=>
+{
+    console.log('Put')
+    const pelicula = peliculaService.UpdatePelicula(req.body, req.params.id)
+    return res.status(200).json(pelicula)
+})
 export default router
