5f52f89a87b7be7ba3c6f2e7cae2e6de0532aecf
FEDERICO CHEDIEX VINER <46916328@ort>
2023-07-14 11:51:15 -0300

    Basta de agregar mas cosas profe :(

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import express from "express";
 import PersonajeRouter from "./src/controllers/PersonajeController.js"
 import PeliculaRouter  from "./src/controllers/PeliculaController.js";
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 const app = express();
 const port = 3000
 
@@ -12,6 +15,7 @@ app.use(express.json());
 
 app.use("/characters", PersonajeRouter);
 app.use("/movies", PeliculaRouter);
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
